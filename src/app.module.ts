411edb9c869e3792e03611b9c74e2aa45889d7e9
mohamad Borna <bornaamir8@gmail.com>
2021-06-25 17:20:17 +0330

    add auth module base satructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeOrm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule,TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TasksModule,TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
